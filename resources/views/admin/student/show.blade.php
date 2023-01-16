11ff024d29ee6c30effb2a0767cb4c52750aab46
Keshav acharya <acharyakeshab20@gmail.com>
2023-01-16 14:25:39 +0545

    Migration updating,renaming tables

--- resources/views/admin/student/show.blade.php
+++ resources/views/admin/student/show.blade.php
@@ -9,22 +9,28 @@
     <div class="container mx-auto max-w-6xl bg-white p-10 mt-6 rounded-3xl">
             <div class="grid grid-cols-2 grid rows-2 gap-4">
 
-                @foreach($studnet->image as $img)
+                @if(!empty($studnet->image))
+                    @foreach($studnet->image as $img)
 
-{{--                    @if($img->extension = 'pdf')--}}
-{{--                    <embed src="{{ url('image/admin/'.$img) }}">--}}
-{{--                    <a href="{{ url('image/admin/'.$img) }}">--}}
-{{--                        PDF FILE--}}
-{{--                    </a>--}}
-                    <a href="{{ url('image/admin/'.$img) }}" target="_blank">{{ url('image/admin/'.$img) }}</a>
-{{--                        <embed src="{{ url('image/admin/'.$img-) }}">--}}
-{{--                    </a>--}}
-{{--                    <iframe src="{{ url('/image/admin/'.$img) }}#toolbar=0" width="100%" height="300px"></iframe>--}}
-{{--                         <iframe src="{{ url('image/admin/'.$img) }}"></iframe>--}}
-{{--                    @elseif($img->extension = 'png')--}}
-{{--                        <img src="{{ url('/image/admin/'.$img) }}" alt="user image" width="500" height="500">--}}
-{{--                    @endif--}}
-                @endforeach
+                        {{--                    @if($img->extension = 'pdf')--}}
+                        {{--                    <embed src="{{ url('image/admin/'.$img) }}">--}}
+                        {{--                    <a href="{{ url('image/admin/'.$img) }}">--}}
+                        {{--                        PDF FILE--}}
+                        {{--                    </a>--}}
+                        <a href="{{ url('image/admin/'.$img) }}" target="_blank">{{ url('image/admin/'.$img) }}</a>
+                        {{--                        <embed src="{{ url('image/admin/'.$img-) }}">--}}
+                        {{--                    </a>--}}
+                        {{--                    <iframe src="{{ url('/image/admin/'.$img) }}#toolbar=0" width="100%" height="300px"></iframe>--}}
+                        {{--                         <iframe src="{{ url('image/admin/'.$img) }}"></iframe>--}}
+                        {{--                    @elseif($img->extension = 'png')--}}
+                        {{--                        <img src="{{ url('/image/admin/'.$img) }}" alt="user image" width="500" height="500">--}}
+                        {{--                    @endif--}}
+                    @endforeach
+                @else
+                        <div class="container mx-auto max-w-6xl">
+                            <h2 class="text-center">File Not found</h2>
+                        </div>
+                @endif
             </div>
     </div>
 </x-admin-layout>
